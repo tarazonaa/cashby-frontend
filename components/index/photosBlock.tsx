e3edcbb9d5666f86c9b7dd6cd935a3a875bbdc84
Andres Tarazona <tarazonaa@andress-mbp.lan>
2022-04-20 16:56:51 -0500

    Initial COmmit

---
+++ components/index/photosBlock.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const PhotoBlock = () => {
+    const CreatePost = (link: string) => {
+
+    }
+    return(
+        <section>
+            <div className="photosContainer" >
+                
+            </div>
+        </section>
+    )
+}
+
+export default PhotoBlock;
