d55f6c4da986f2cfa8e91077a7e8645b4f4bfb25
Tarazonaa <andres.tara.so@gmail.com>
2022-09-26 09:48:10 -0500

    Se me olvido commitear el education page

--- pages/educationPage.tsx
+++ pages/educationPage.tsx
@@ -1,19 +1,38 @@
-import type {NextPage} from "next";
+import type {NextComponentType, NextPage} from "next";
 import { StaticImageData } from "next/image";
 import Image from "next/image";
 
+//Images 
+import ArtOfWar1 from "../assets/Images/Education/ArtofWar1.jpeg";
+import Marketing1 from "../assets/Images/Education/Marketing1.jpeg";
+import Prince2 from "../assets/Images/Education/Prince2.jpeg";
+import ArtOfWar2 from "../assets/Images/Education/ArtofWar2.jpeg";
+
 
 const educationPage: NextPage = () => {
-    const createBook = (img: StaticImageData, altText: string): JSX.Element => {
+    const createBook = (img: StaticImageData, altText: string, name: string): JSX.Element => {
         return(
-            <div>
-                <Image src={img.src} height={img.height} width={img.width} alt={altText}/>
+            <div className="educationBook">
+                <h2>{name}</h2>
+                <Image src={img.src} height={400} width={300} alt={altText}/>
+            </div>
+        )
+    }
+    const createBanner = (img: StaticImageData, altText: string, name: string): JSX.Element => {
+        return(
+            <div className="educationBanner">
+                <h2>{name}</h2>
+                <Image src={img.src} height={350} width={675} alt={altText}/>
             </div>
         )
     }
     return(
-        <div>
-            {createBook({src:"/../assets/Images/investing.png", height:200, width:200}, "Image")}
+        <div className="educationPage">
+            {createBanner(Prince2, "Prince2", "Marketing - Google")}
+            <div className="educationBooks">
+                {createBook(ArtOfWar1, "ArtOfWar1", "The Art of War - Sun Tzu")}
+                {createBook(Marketing1, "Marketing1", "Marketing - Google")}
+            </div>
         </div>
     )
 }
