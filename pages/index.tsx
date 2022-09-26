6dc6c8ffb7a2d7d9ed6cddb18aa52c5614473174
Tarazonaa <andres.tara.so@gmail.com>
2022-09-26 10:26:35 -0500

    Tailwind init (Faltan configs)

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,13 @@ import type { NextPage } from "next";
 // Components
 
 const Home: NextPage = () => {
-  return (<div>Hello World</div>);
+  return (<div className="flex min-h-screen justify-center" >
+    <div className="LandingContent">
+        <a>
+            <button className="LandingButton">Home</button>
+        </a>
+    </div>
+  </div>);
 };
 
 export default Home;
