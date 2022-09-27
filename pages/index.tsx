6910477cf40bc4aba32d490300003e740b8b2af9
Tarazonaa <andres.tara.so@gmail.com>
2022-09-26 21:56:24 -0500

    TAILWIND WORKS

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,11 +3,9 @@ import type { NextPage } from "next";
 // Components
 
 const Home: NextPage = () => {
-  return (<div className="justify-center" >
-    <div className="LandingContent">
-        <a>
-            <button className="bg-red-500">Home</button>
-        </a>
+  return (<div className="justify-center flex flex-col">
+    <div className="flex flex-col justify-center align-center bg-zinc-500 h-screen">
+        <button className="bg-gray-200 p-24 rounded hover:bg-gray-400"><a className="text-zinc-500 text-3xl hover:text-zinc-300" href="">Home</a></button>
     </div>
   </div>);
 };
